cf252e1656108aa9b28fe1952cdbc952332755cb
Armando Meza Leyva <armezaleyva@gmail.com>
2018-11-15 08:22:38 -0700

    Add project files.

---
+++ Herencia/AnimalTerrestre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia {
+    public class AnimalTerrestre : Animal {
+        public void Caminar() {
+            Console.WriteLine("Hola soy un " + Nombre + " y estoy caminando");
+        }
+    }
+}
